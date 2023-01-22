1ef99b7021c09e8aaa0e87a124e4b18ae33150fa
Curtis Henley <curtisdhenley@gmail.com>
2023-01-22 16:29:04 -0500

    still working on table display logic

--- js/site.js
+++ js/site.js
@@ -1,17 +1,18 @@
-function calculateAmounts() {
+function getUserInput() {
     let loanAmount = document.getElementById('loanAmount').value;
     let term = document.getElementById('term').value;
     let rate = document.getElementById('rate').value;
 
-    loanAmount = parseInt(loanAmount)
-    term = parseInt(term);
-    rate = parseFloat(rate);
+    calculateAmounts(loanAmount, term, rate);
+}
+
+function calculateAmounts(amount, term, rate) {
 
     // total monthly payment = (amount loaned) * (rate/1200)/(1-(1 + rate/1200)(-number of months))
-    let monthlyPayment = (loanAmount * (rate / 1200) / (1 - (1 + rate / 1200) ** (-term)))
+    let monthlyPayment = (amount * (rate / 1200) / (1 - (1 + rate / 1200) ** (-term)));
 
     // remaining balance before the very first month equals the amount of the loan
-    let remainingBalance = loanAmount;
+    let remainingBalance = amount;
 
     // interest payment = previous remaining balance * rate/1200
     let interestPayment = remainingBalance * (rate / 1200)
@@ -23,21 +24,21 @@ function calculateAmounts() {
     let newRemainBalance = remainingBalance - principalPayment;
 
     let loanParts = {
-        'Loan Amount': loanAmount,
+        'Loan Amount': amount,
         'Loan Term': term,
-        'Interest Rate': rate,
+        'Loan Interest Rate': rate,
         'Monthly Payment': monthlyPayment,
         'Remaining Balance': remainingBalance,
         'Interest Payment': interestPayment,
         'Principal Payment': principalPayment,
         'New Remaining Balance': newRemainBalance
     }
+    displayBalances(loanParts);
+    displayPaymentData(loanParts);
 
-    return loanParts;
 }
 
-function displayBalances() {
-    let balancesArr = calculateAmounts();
+function displayBalances(balancesArr) {
 
     document.getElementById('monthly-payment').innerHTML = balancesArr['Monthly Payment'].toLocaleString(
         'en-US', {
@@ -63,13 +64,9 @@ function displayBalances() {
         minimumFractionDigits: 0
     }
     );
-
-    displayPaymentData();
 }
 
-function displayPaymentData() {
-    
-    let balancesArr = calculateAmounts();
+function displayPaymentData(balancesArr) {
     let tableBody = document.getElementById('mortgageTableBody');
     const tableRowTemplate = document.getElementById('mortgageTableRowTemplate');
 
@@ -83,20 +80,31 @@ function displayPaymentData() {
         tableCells[0].innerHTML = i;
 
         // payment
-        tableCells[1].innerHTML = balancesArr['Monthly Payment'];
+        tableCells[1].innerHTML = balancesArr['Monthly Payment'].toLocaleString(
+            'en-US', {
+            maximumFractionDigits: 0,
+            minimumFractionDigits: 0
+        }
+        );
+        
+        // (balancesArr['Loan Amount'] * (balancesArr['Loan Interest Rate'] / 1200) / (1 - (1 + balancesArr['Loan Interest Rate'] / 1200) ** (-balancesArr['Loan Terms'])));
 
         // principal
-        tableCells[2].innerHTML = balancesArr['Monthly Payment'] - balancesArr['Interest Payment'];
+        balancesArr['Principal Payment'] = (balancesArr['Monthly Payment'] - (balancesArr['Remaining Balance'] * (balancesArr['Loan Interest Rate'] / 1200)));
+        tableCells[2].innerHTML = balancesArr['Principal Payment'];
 
         // interest
-        tableCells[3].innerHTML = balancesArr['Remaining Balance'] * (rate / 1200);
+        balancesArr['Interest Payment'] = balancesArr['Remaining Balance'] * (balancesArr['Loan Interest Rate'] / 1200);
+        tableCells[3].innerHTML = balancesArr['Interest Payment'];
 
         // total interest
-        tableCells[4].innerHTML = tableCells[3].innerHTML + (balancesArr['Remaining Balance'] * (rate / 1200));
+        tableCells[4].innerHTML = balancesArr['Interest Payment'];
 
         // balance
-        tableCells[5].innerHTML = (balancesArr['Remaining Balance'] - tableCells[2].innerHTML);
+        balancesArr['New Remaining Balance'] = balancesArr['Remaining Balance'] - balancesArr['Principal Payment'];
+        tableCells[5].innerHTML = balancesArr['New Remaining Balance']; 
 
         tableBody.appendChild(eventRow);
+        balancesArr['Remaining Balance'] = balancesArr['New Remaining Balance'];
     }
 }
